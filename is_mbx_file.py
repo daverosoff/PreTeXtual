5e0bebfa377362aef448496babeedff47eefcae1
Dave Rosoff <drosoff@collegeofidaho.edu>
2016-06-15 06:35:19 -0600

    A first attempt

--- is_mbx_file.py
+++ is_mbx_file.py
@@ -24,6 +24,11 @@ import re
 # except:
 #     from .settings import get_setting
 
+try:
+    from mbx_ref_completions import get_setting
+except:
+    from .mbx_ref_completions import get_setting
+
 if sys.version_info < (3, 0):
     strbase = basestring
 else:
@@ -31,10 +36,11 @@ else:
 
 def get_mbx_extensions():
     # view = sublime.active_window().active_view()
-    # tex_file_exts = get_setting('tex_file_exts', ['.tex'])
+    # You can change settings in Packages/User/MBXTools
+    mbx_file_exts = get_setting('mbx_file_exts', ['.mbx'])
 
-    return ['mbx']
-    # return [s.lower() for s in set(tex_file_exts)]
+    # return ['mbx']
+    return [s.lower() for s in set(mbx_file_exts)]
 
 def is_mbx_file(file_name):
     if not isinstance(file_name, strbase):
