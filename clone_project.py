cb50419f66f96c8617c6847ddf0bef414a1d79cd
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-04-12 12:30:28 -0600

    it's aliiiiiiiive

--- clone_project.py
+++ clone_project.py
@@ -3,28 +3,20 @@ import sublime_plugin
 import subprocess
 
 class CloneProjectCommand(sublime_plugin.TextCommand):
-    # def clone_url_and_add_to_project(st):
-    #     proj = st.split('/')[-1].split('.')[0]
-    #     newfolder = 'C:\\Users\\drosoff\\pv\\' + proj
-    #     pdata = win.project_data()
-    #     if pdata['folders']:
-    #         folders = pdata['folders']
-    #         folders.append({'path': newfolder})
-    #         pdata['folders'] = folders
-    #     else:
-    #         win.set_project_data(pdata)
 
     def run(self, edit):
-        #self.view.insert(edit, 0, "Hello, World!")
-        # win = sublime.active_window()
+        # self.view.insert(edit, 0, "Hello, World!")
+        win = sublime.active_window()
+        print("Yes!")
         vu = self.view
         win = vu.window()
-        win.show_input_panel("Enter a URL to clone from:", "https://github.com/", lambda st: subprocess.call(["git", "clone", st]), None, None)
-        # win.show_input_panel(
-        #     "Enter a URL to clone from:",
-        #     "https://github.com/",
-        #     lambda st: subprocess.call(["git", "clone", st]),
-        #     # self.clone_url_and_add_to_project,
-        #     None,
-        #     None
-        # )
+        # win.show_input_panel("Enter a URL to clone from:", "https://github.com/", lambda st: subprocess.call(["git", "clone", st]), None, None)
+        win.show_input_panel(
+            "Enter a URL to clone from:",
+            "https://github.com/",
+            lambda st: subprocess.call(["git", "clone", st, win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]]),
+            # lambda st: print(win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]),
+            # self.clone_url_and_add_to_project,
+            None,
+            None
+        )
