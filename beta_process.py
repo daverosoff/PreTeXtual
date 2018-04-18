ba9b51e9a2f085a1b869dc0bb4e99987914866d3
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-04-17 23:29:38 -0600

    migrate to using settings and custom target

--- beta_process.py
+++ beta_process.py
@@ -1,6 +1,8 @@
 import sublime
 import sublime_plugin
 import subprocess
+from .get_setting import get_setting
+import os, time
 
 class BetaCommand(sublime_plugin.WindowCommand):
     # def _verbose(msg):
@@ -10,39 +12,47 @@ class BetaCommand(sublime_plugin.WindowCommand):
     #     if args.verbose and args.verbose >= 1:
     #         print('MBX: {}'.format(msg))
 
-    def run(self, cmd, **kwargs):
+    # get project settings
+    # xinclude: boolean
+    # output: string (absolute path name for vagrant)
+    # stringparams: dict{string:string}
+
+    def run(self, cmd, format, **kwargs):
+        filename = self.window.active_view().file_name()
+        filepath = os.path.dirname(filename)
+
+        vagrantpath = get_setting('vagrantpath', "C:/HashiCorp/Vagrant/bin/vagrant.exe")
+        vagrantroot = get_setting('vagrantroot', "C:/PreTeXt/")
+        vagrantcommand = get_setting('vagrantcommand', None)
+        xinclude = get_setting('xinclude', False)
+        stringparam = get_setting('stringparam', {})
+        pretext_root_file = get_setting('pretext_root_file', filename)
+        pretext_output = get_setting('pretext_output', filepath)
+        pretext_images = get_setting('pretext_images', os.path.join(filepath, "images"))
+        pretext_stylesheets = get_setting('pretext_stylesheets', {})
+
+        if not pretext_stylesheets:
+            print("Cannot find PreTeXt stylesheets, check settings :(")
+            return
         if cmd == "xsltproc":
-            prefix = "vagrant ssh --command \"xsltproc "
-            print("Invoking xsltproc")
-            # last 2 args are XSL stylesheet and PTX source
-            # process kwargs
-            suffix = "{} {}\"".format(kwargs['xsl'], kwargs['pretext'])
-            for k, v in kwargs.items():
-                if k == "xinclude":
-                    prefix = prefix + "--xinclude " if v else prefix
-                if k == "output":
-                    # validating: either through vagrant (slow) or via text xfrm?
-                    # for now, do nothing
-                    # import os.path
-                    # if not(os.path.isdir(v)):
-                        # raise ValueError("directory {} does not exist".format(v))
-                    if v[-1] != '/':
-                        v = v + '/'
-                    prefix = prefix + "--output {} ".format(v)
-                # receive stringparam as a dict of k-v pairs
-                if k == "stringparam":
-                    for param, val in v.items():
-                        print("processing stringparam {} {}".format(param, val))
-                        prefix = prefix + "--stringparam {} {} ".format(param, val)
-            cmd_string = prefix + suffix
+            print("Invoking xsltproc...{}".format(time.gmtime(time.time())))
+            if xinclude:
+                xp_prefix = "xsltproc --xinclude --output {} ".format(pretext_output)
+            else:
+                xp_prefix = "xsltproc --output {} ".format(pretext_output)
+            xp_sps = ""
+            if stringparam:
+                for k, v in stringparam.items():
+                    xp_sps = "{} --stringparam {} \\\"{}\\\"".format(xp_sps, k, v)
+            xp_suffix = " {} {}".format(pretext_stylesheets[format], pretext_root_file)
+            cmd_string = "{} \"{}\"".format(vagrantcommand, xp_prefix + xp_sps + xp_suffix)
             print("Calling: {}".format(cmd_string))
             # subprocess.run is not available in python 3.3.6 which ST3 uses as of 3162
             try:
                 result = subprocess.check_output(cmd_string, shell=True)
             except subprocess.CalledProcessError as e:
-                print(e.output)
-                print("Build failed.")
-                return
+                print("Build warning (nonzero exit code): {}".format(e.output))
+                pass
             print("Build complete.")
 
 
