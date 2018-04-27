f65c464834a8f11bd0d8fcbdba4df8fca095bd6d
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-04-26 23:43:28 -0600

    Popen with output, add the folder when done

--- initialize_pretext_vagrant.py
+++ initialize_pretext_vagrant.py
@@ -47,9 +47,33 @@ class InitializePretextVagrantCommand(sublime_plugin.WindowCommand):
             base_url = "https://raw.githubusercontent.com/daverosoff/pretext-vagrant/master/Vagrantfile-PreTeXt"
             url_exts = ["", "-lite", "-barebones", "-no-images"]
             box_name = "daverosoff/pretext" + url_exts[n]
-            subprocess.call("vagrant init {}".format(box_name), cwd=loc)
-            subprocess.call("vagrant up", cwd=loc)
-            subprocess.call("vagrant suspend", cwd=loc)
+            proc = subprocess.Popen("vagrant init {}".format(box_name), cwd=loc,
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            while proc.poll() is None:
+                try:
+                    data = proc.stdout.readline().decode(encoding="UTF-8")
+                    print(data, end="")
+                except:
+                    return
+            # subprocess.call("vagrant init {}".format(box_name), cwd=loc)
+            proc = subprocess.Popen("vagrant up", cwd=loc,
+                )
+            while proc.poll() is None:
+                try:
+                    data = proc.stdout.readline().decode(encoding="UTF-8")
+                    print(data, end="")
+                except:
+                    return
+            # subprocess.call("vagrant init {}".format(box_name), cwd=loc)
+            proc = subprocess.Popen("vagrant suspend", cwd=loc,
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            while proc.poll() is None:
+                try:
+                    data = proc.stdout.readline().decode(encoding="UTF-8")
+                    print(data, end="")
+                except:
+                    return
+            # subprocess.call("vagrant init {}".format(box_name), cwd=loc)
             # with urllib.request.urlopen(base_url + url_exts[n]) as url:
             #     try:
             #         with open(loc, 'wb') as u:
@@ -80,3 +104,8 @@ class InitializePretextVagrantCommand(sublime_plugin.WindowCommand):
                 #<selected_index>,
                 #<on_highlighted>
             )
+        projdata = self.window.project_data() or {}
+        if not projdata["folders"]:
+            projdata["folders"] = []
+        projdata["folders"].append({"path": "C:/PreTeXt"})
+        self.window.set_project_data(projdata)
