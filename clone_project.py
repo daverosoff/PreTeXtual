2bf66c2d58fabc14667c16ad2eade6c279ee87bf
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-04-26 23:42:53 -0600

    Remove trace statement

--- clone_project.py
+++ clone_project.py
@@ -7,7 +7,6 @@ class CloneProjectCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # self.view.insert(edit, 0, "Hello, World!")
         win = sublime.active_window()
-        print("Yes!")
         vu = self.view
         win = vu.window()
         # win.show_input_panel("Enter a URL to clone from:", "https://github.com/", lambda st: subprocess.call(["git", "clone", st]), None, None)
