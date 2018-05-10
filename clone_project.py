2fae66151472b2971cc015ae3c53961c08458ab5
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-05-10 10:59:03 -0600

    reflow long line

--- clone_project.py
+++ clone_project.py
@@ -6,13 +6,9 @@ class CloneProjectCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         # self.view.insert(edit, 0, "Hello, World!")
-        win = sublime.active_window()
-        vu = self.view
-        win = vu.window()
-        win.show_input_panel(
-            "Enter a URL to clone from:",
-            "https://github.com/",
-            lambda st: subprocess.call(["git", "clone", st, win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]]),
-            None,
-            None
+        win = sublime.active_window() vu = self.view win = vu.window()
+        win.show_input_panel("Enter a URL to clone from:",
+            "https://github.com/", lambda st: subprocess.call(["git", "clone",
+            st, win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]]),
+            None, None
         )
