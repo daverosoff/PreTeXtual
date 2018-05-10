6ccb09b3fe5d5ff4f1bbaed3d9085c4006a57d1d
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-05-10 10:55:12 -0600

    delete old code

--- clone_project.py
+++ clone_project.py
@@ -9,13 +9,10 @@ class CloneProjectCommand(sublime_plugin.TextCommand):
         win = sublime.active_window()
         vu = self.view
         win = vu.window()
-        # win.show_input_panel("Enter a URL to clone from:", "https://github.com/", lambda st: subprocess.call(["git", "clone", st]), None, None)
         win.show_input_panel(
             "Enter a URL to clone from:",
             "https://github.com/",
             lambda st: subprocess.call(["git", "clone", st, win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]]),
-            # lambda st: print(win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]),
-            # self.clone_url_and_add_to_project,
             None,
             None
         )
