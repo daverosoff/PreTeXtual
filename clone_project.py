3f4e801a75900fdb7ff20326d5d27c724948b76e
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-06-19 21:09:58 -0600

    Unclear how this got corrupted

--- clone_project.py
+++ clone_project.py
@@ -6,7 +6,9 @@ class CloneProjectCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         # self.view.insert(edit, 0, "Hello, World!")
-        win = sublime.active_window() vu = self.view win = vu.window()
+        # win = sublime.active_window()
+        vu = self.view
+        win = vu.window()
         win.show_input_panel("Enter a URL to clone from:",
             "https://github.com/", lambda st: subprocess.call(["git", "clone",
             st, win.folders()[0] + "/" + st.split("/")[-1].split(".")[0]]),
